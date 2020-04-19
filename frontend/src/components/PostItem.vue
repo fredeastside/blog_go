8d509801e2b0c1161a599f9ff0e0a7527851ed63
fredrsf <kaznakhovskiy@gmail.com>
2020-04-19 10:01:57 +0300

    add menu for mobiles

--- frontend/src/components/PostItem.vue
+++ frontend/src/components/PostItem.vue
@@ -1,26 +1,47 @@
 <template>
-  <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-    <header>
-      <h1 class="posttitle" itemprop="name headline">
-        {{ post.name }}
-      </h1>
-      <div class="meta">
-        <!--span class="author" itemprop="author" itemscope itemtype="http://schema.org/Person">
-                    <span itemprop="name">Author</span>
-                </span-->
-        <time :datetime="post.created" itemprop="datePublished">
-          {{ post.created | moment("DD MMM YYYY") }}
-        </time>
+  <fragment>
+    <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
+      <header>
+        <h1 class="posttitle" itemprop="name headline">
+          {{ post.name }}
+        </h1>
+        <div class="meta">
+          <!--span class="author" itemprop="author" itemscope itemtype="http://schema.org/Person">
+                      <span itemprop="name">Author</span>
+                  </span-->
+          <time :datetime="post.created" itemprop="datePublished">
+            {{ post.created | moment("DD MMM YYYY") }}
+          </time>
+        </div>
+      </header>
+      <div class="content" itemprop="articleBody" v-html="post.content"></div>
+    </article>
+    <div id="footer-post-container">
+      <div id="footer-post">
+
+        <div id="nav-footer" style="display: none">
+          <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+          </ul>
+        </div>
+
+        <div id="actions-footer">
+          <a id="menu" class="icon" href="#" v-on:click="toggleMenu($event)">
+            <font-awesome-icon :icon="['fas', 'bars']" size="lg" aria-hidden="true"
+            /> Menu</a>
+        </div>
+
       </div>
-    </header>
-    <div class="content" itemprop="articleBody" v-html="post.content"></div>
-  </article>
+    </div>
+  </fragment>
 </template>
 
 <script>
 import http from "@/http";
 import "highlight.js/styles/atom-one-dark.css";
-import hljs from "highlight.js"
+import hljs from "highlight.js";
+import $ from "jquery";
 
 export default {
   name: "PostItem",
@@ -47,6 +68,11 @@ export default {
       document.querySelectorAll('pre code').forEach((block) => {
         hljs.highlightBlock(block);
       });
+    },
+    toggleMenu(event) {
+      event.preventDefault();
+      $('#nav-footer').toggle();
+      return false;
     }
   }
 };
